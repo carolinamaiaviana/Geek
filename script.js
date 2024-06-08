3763d215ca116374bc1c0abe74b9487454e2ba83
carolinamaiaviana <carolinabastosmaia@outlook.com>
2024-06-08 15:34:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,399 @@
+var arrayBox1 = [
+{
+    Image:"https://images.unsplash.com/photo-1673255745677-e36f618550d1?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Machine Learning for AI development",
+    Time:"3h20min",
+    Level:"Advanced",
+    Note:3,
+    Ratings:1.124,
+    Price:900,
+    UserImage:"https://randomuser.me/api/portraits/men/46.jpg",
+    Name:"Andre Braugher"
+},
+
+{
+    Image:"https://images.unsplash.com/photo-1548502499-ef49e8cf98d4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Professional photographer guide",
+    Time:"5h45min",
+    Level:"Advanced",
+    Note:5,
+    Ratings:1.652,
+    Price:650,
+    UserImage:"https://randomuser.me/api/portraits/women/42.jpg",
+    Name:"Melissa Fumero"
+},
+{
+    Image:"https://images.unsplash.com/photo-1619410283995-43d9134e7656?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"React.Js Framework from scratch",
+    Time:"4h50min",
+    Level:"Beginner",
+    Note:5,
+    Ratings:2.138,
+    Price:840,
+    UserImage:"https://randomuser.me/api/portraits/women/44.jpg",
+    Name:"Rosa Diaz"
+},
+
+{
+    Image:"https://images.unsplash.com/photo-1598946114829-162ee19ac506?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Virtual Private Networks (VPN)",
+    Time:"3h30min",
+    Level:"Intermediate",
+    Note:4,
+    Ratings:1.143,
+    Price:720,
+    UserImage:"https://randomuser.me/api/portraits/men/34.jpg",
+    Name:"Ang Bender"
+},
+]
+
+var arrayBox2 = [
+    {
+        Image:"https://plus.unsplash.com/premium_photo-1685086785054-d047cdc0e525?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Title:"Introduction to Computer Science",
+        Time:"2h20min",
+        Level:"Intermediate",
+        Note:5,
+        Ratings:1.136,
+        Price:500,
+        UserImage:"https://randomuser.me/api/portraits/women/23.jpg",
+        Name:"Madyson Carpenter"
+    },
+    {
+        Image:"https://plus.unsplash.com/premium_photo-1661382011487-cd3d6b1d9dff?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Title:"UX & Design Complete Guide",
+        Time:"5h35min",
+        Level:"Beginner",
+        Note:3,
+        Ratings:1.267,
+        Price:435,
+        UserImage:"https://randomuser.me/api/portraits/men/1.jpg",
+        Name:"Adam Lagivne"
+    },
+    {
+        Image:"https://images.unsplash.com/photo-1609619385002-f40f1df9b7eb?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Title:"Adobe Photoshop 2018 from scratch",
+        Time:"3h50min",
+        Level:"Beginner",
+        Note:5,
+        Ratings:2.138,
+        Price:670,
+        UserImage:"https://randomuser.me/api/portraits/women/2.jpg",
+        Name:"Gina Linetti"
+    },
+    {
+        Image:"https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Title:"Algorithms and Programming Logics",
+        Time:"4h30min",
+        Level:"Intermediate",
+        Note:4,
+        Ratings:2.354,
+        Price:850,
+        UserImage:"https://randomuser.me/api/portraits/men/9.jpg",
+        Name:"Brad Schneider"
+    }
+    ]
+
+var arrayBox3 = [
+{
+    Image:"https://images.unsplash.com/photo-1662026911591-335639b11db6?q=80&w=862&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"MySQL Programming Complete Guide",
+    Time:"6h40min",
+    Level:"Intermediate",
+    Note:4,
+    Ratings:198,
+    Price:780,
+    UserImage:"https://randomuser.me/api/portraits/women/26.jpg",
+    Name:"Haley Bayle"
+},
+{
+    Image:"https://images.unsplash.com/photo-1492619375914-88005aa9e8fb?q=80&w=865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Video Editing using Adobe Premiere",
+    Time:"5h40min",
+    Level:"Advanced",
+    Note:5,
+    Ratings:267,
+    Price:435,
+    UserImage:"https://randomuser.me/api/portraits/men/1.jpg",
+    Name:"Adam Lagivne"
+},
+{
+    Image:"https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Data Science with Python and Flask",
+    Time:"4h30min",
+    Level:"Advanced",
+    Note:4,
+    Ratings:189,
+    Price:399,
+    UserImage:"https://randomuser.me/api/portraits/men/33.jpg",
+    Name:"James Smith"
+},
+{
+    Image:"https://plus.unsplash.com/premium_photo-1661281412140-dfb328ae967b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Graphic Design Basics with Corel Draw",
+    Time:"5h",
+    Level:"Beginner",
+    Note:3,
+    Ratings:112,
+    Price:249,
+    UserImage:"https://randomuser.me/api/portraits/men/19.jpg",
+    Name:"Alex Turner"
+}
+]
+
+var arrayBox4 = [
+{
+    Image:"https://images.unsplash.com/photo-1683721003111-070bcc053d8b?q=80&w=1100&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Social Media Marketing Essentials",
+    Time:"1h40min",
+    Level:"Beginner",
+    Note:4,
+    Ratings:1.472,
+    Price:250,
+    UserImage:"https://randomuser.me/api/portraits/women/65.jpg",
+    Name:"Olivia Turner"
+},
+{
+    Image:"https://images.unsplash.com/photo-1516724562728-afc824a36e84?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Introduction to Portrait Photography",
+    Time:"1h45min",
+    Level:"Advanced",
+    Note:5,
+    Ratings:2.209,
+    Price:360,
+    UserImage:"https://randomuser.me/api/portraits/men/10.jpg",
+    Name:"Ethan Miller"
+},
+{
+    Image:"https://images.unsplash.com/photo-1632882765546-1ee75f53becb?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"HTML5 Basics for Web Development",
+    Time:"2h15min",
+    Level:"Beginner",
+    Note:3,
+    Ratings:165,
+    Price:450,
+    UserImage:"https://randomuser.me/api/portraits/men/75.jpg",
+    Name:"Jake Sander"
+},
+{
+    Image:"https://images.unsplash.com/photo-1511376777868-611b54f68947?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Title:"Mobile App Development with React Native",
+    Time:"2h45min",
+    Level:"Intermediate",
+    Note:5,
+    Ratings:216,
+    Price:359,
+    UserImage:"https://randomuser.me/api/portraits/women/90.jpg",
+    Name:"Emma Harris"
+}
+]
+var estrelaAmarela = '<i class="fa fa-star mt-1" style="color: yellow;" aria-hidden="true"></i>'
+var estrelaCinza = '<i class="fa fa-star mt-1" style="color: gray; opacity: 50%;" aria-hidden="true"></i>'
+
+var box1 = document.getElementById("box1")
+var box2 = document.getElementById("box2")
+var box3 = document.getElementById("box3")
+var box4 = document.getElementById("box4")
+
+
+arrayBox1.map(item => {
+    box1.innerHTML += ` <div id="card-single" class="card mt-3" style="width: 15rem;">
+    <div class="overflow-hidden"  style="width: 100%;height: 120px;">
+      <img class="card-img-top img-fluid w-full" style="object-fit: cover;"  src="${item.Image}" >
+    </div>
+    <div class="card-body d-flex flex-column justify-content-between">
+    <p class="card-text font-monospace .text-wrap" style="font-size: 15px;">${item.Title}</p>
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-wrap">
+                <div class=" d-flex justify-content-start">
+                <i class="fa fa-clock-o " style="font-size: 15px; margin-top: 5px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1 mt-1" style="font-size: 15px;">${item.Time}</span>
+                </div>
+    
+                <div class="d-flex align-items-center">
+                <i class="fa fa-bar-chart ms-4" style="font-size:14px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1" style="font-size: 14px;">${item.Level}</span>
+                </div>
+            </div>
+            <div class="d-flex flex-column">
+                <div class="d-flex">
+                ${estrelaAmarela.repeat(item.Note) +
+                estrelaCinza.repeat(5-item.Note)}
+                <span class="fw-light ms-1 mt-1" style="font-size: 14px;">(${item.Ratings})</span>
+                </div>
+    
+                <div class="d-flex">
+                    <p class="fw-bold">$${item.Price}</p>
+                    <span class="fw-light ms-1 mt-1" style="font-size: 14px;">/month</span>
+                </div>
+                <hr class="m-0">
+            </div>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+            <div>
+            <img src=${item.UserImage} class="rounded-circle mt-1" style="width:30px;height: 30px;" alt="">
+            </div>
+            <div>
+            <span class="fw-light ms-1 mt-2 " style="font-size: 14px;">${item.Name}</span>
+            </div>
+            </div>
+            <div>
+                <i class="fa fa-bookmark-o" style="margin-right:0px; margin-top: 6px;" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    </div>`
+})
+
+arrayBox2.map(item => {
+    box2.innerHTML += ` <div id="card-single" class="card mt-3" style="width: 15rem;">
+    <div class="overflow-hidden"  style="width: 100%;height: 120px;">
+      <img class="card-img-top img-fluid w-full" style="object-fit: cover;"  src="${item.Image}" >
+    </div>
+    <div class="card-body">
+    <p class="card-text font-monospace .text-wrap" style="font-size: 15px;">${item.Title}</p>
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-wrap">
+                <div class=" d-flex justify-content-start">
+                <i class="fa fa-clock-o " style="font-size: 15px; margin-top: 5px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1 mt-1" style="font-size: 15px;">${item.Time}</span>
+                </div>
+    
+                <div class="d-flex align-items-center">
+                <i class="fa fa-bar-chart ms-4" style="font-size:14px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1" style="font-size: 14px;">${item.Level}</span>
+                </div>
+            </div>
+            <div class="d-flex flex-column">
+                <div class="d-flex">
+                ${estrelaAmarela.repeat(item.Note) +
+                estrelaCinza.repeat(5-item.Note)}
+                <span class="fw-light ms-1 mt-1" style="font-size: 14px;">(${item.Ratings})</span>
+                </div>
+    
+                <div class="d-flex">
+                    <p class="fw-bold">$${item.Price}</p>
+                    <span class="fw-light ms-1 mt-1" style="font-size: 14px;">/month</span>
+                </div>
+                <hr class="m-0">
+            </div>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+            <div>
+            <img src=${item.UserImage} class="rounded-circle mt-1" style="width:30px;height: 30px;" alt="">
+            </div>
+            <div>
+            <span class="fw-light ms-1 mt-2 " style="font-size: 14px;">${item.Name}</span>
+            </div>
+            </div>
+            <div>
+                <i class="fa fa-bookmark-o" style="margin-right:0px; margin-top: 6px;" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    </div>`
+})
+
+arrayBox3.map(item => {
+    box3.innerHTML += ` <div id="card-single" class="card mt-3" style="width: 15rem;">
+    <div class="overflow-hidden"  style="width: 100%;height: 120px;">
+      <img class="card-img-top img-fluid w-full" style="object-fit: cover;"  src="${item.Image}" >
+    </div>
+    <div class="card-body d-flex flex-column justify-content-between">
+    <p class="card-text font-monospace .text-wrap" style="font-size: 15px;">${item.Title}</p>
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-wrap">
+                <div class=" d-flex justify-content-start">
+                <i class="fa fa-clock-o " style="font-size: 15px; margin-top: 5px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1 mt-1" style="font-size: 15px;">${item.Time}</span>
+                </div>
+    
+                <div class="d-flex align-items-center">
+                <i class="fa fa-bar-chart ms-4" style="font-size:14px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1" style="font-size: 14px;">${item.Level}</span>
+                </div>
+            </div>
+            <div class="d-flex flex-column">
+                <div class="d-flex">
+                ${estrelaAmarela.repeat(item.Note) +
+                estrelaCinza.repeat(5-item.Note)}
+                <span class="fw-light ms-1 mt-1" style="font-size: 14px;">(${item.Ratings})</span>
+                </div>
+    
+                <div class="d-flex">
+                    <p class="fw-bold">$${item.Price}</p>
+                    <span class="fw-light ms-1 mt-1" style="font-size: 14px;">/month</span>
+                </div>
+                <hr class="m-0">
+            </div>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+            <div>
+            <img src=${item.UserImage} class="rounded-circle mt-1" style="width:30px;height: 30px;" alt="">
+            </div>
+            <div>
+            <span class="fw-light ms-1 mt-2 " style="font-size: 14px;">${item.Name}</span>
+            </div>
+            </div>
+            <div>
+                <i class="fa fa-bookmark-o" style="margin-right:0px; margin-top: 6px;" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    </div>`
+})
+
+arrayBox4.map(item => {
+    box4.innerHTML += ` <div id="card-single" class="card mt-3" style="width: 15rem;">
+    <div class="overflow-hidden"  style="width: 100%;height: 120px;">
+      <img class="card-img-top img-fluid w-full" style="object-fit: cover;"  src="${item.Image}" >
+    </div>
+    <div class="card-body d-flex flex-column justify-content-between">
+    <p class="card-text font-monospace .text-wrap" style="font-size: 15px;">${item.Title}</p>
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-wrap">
+                <div class=" d-flex justify-content-start">
+                <i class="fa fa-clock-o " style="font-size: 15px; margin-top: 5px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1 mt-1" style="font-size: 15px;">${item.Time}</span>
+                </div>
+    
+                <div class="d-flex align-items-center">
+                <i class="fa fa-bar-chart ms-4" style="font-size:14px;" aria-hidden="true"></i>
+                <span class="fw-light ms-1" style="font-size: 14px;">${item.Level}</span>
+                </div>
+            </div>
+            <div class="d-flex flex-column">
+                <div class="d-flex">
+                ${estrelaAmarela.repeat(item.Note) +
+                estrelaCinza.repeat(5-item.Note)}
+                <span class="fw-light ms-1 mt-1" style="font-size: 14px;">(${item.Ratings})</span>
+                </div>
+    
+                <div class="d-flex">
+                    <p class="fw-bold">$${item.Price}</p>
+                    <span class="fw-light ms-1 mt-1" style="font-size: 14px;">/month</span>
+                </div>
+                <hr class="m-0">
+            </div>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+            <div>
+            <img src=${item.UserImage} class="rounded-circle mt-1" style="width:30px;height: 30px;" alt="">
+            </div>
+            <div>
+            <span class="fw-light ms-1 mt-2 " style="font-size: 14px;">${item.Name}</span>
+            </div>
+            </div>
+            <div>
+                <i class="fa fa-bookmark-o" style="margin-right:0px; margin-top: 6px;" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+    </div>`
+})
+//renomear a array para arrayBox1 e fazer modificações
+// segunda array arrayBox2 e jogar no box2
